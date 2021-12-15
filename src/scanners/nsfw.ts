4dcfbcb797c7996bef3a33252d79259cd15214c5
Azury <azury.wtf@gmail.com>
2021-12-15 19:39:34 +0100

    fixed `nsfw` scanning module

--- src/scanners/nsfw.ts
+++ src/scanners/nsfw.ts
@@ -1,11 +1,19 @@
 import fetch from 'node-fetch'
+import formData from 'form-data'
 
 export default async (stream: any) => {
   try {
-    const res = await fetch('https://api.unscan.co/nsfw', { method: 'POST', body: JSON.stringify({ file: stream }) })
+    const form = new formData()
+    form.append('file', stream)
+
+    const res = await fetch('https://api.unscan.co/nsfw', {
+      method: 'POST',
+      body: form
+    })
     const data: any = await res.json()
+
     return data.nsfw
-  } catch (err: any) {
-    return null
+  } catch (err) {
+    throw err
   }
 }
